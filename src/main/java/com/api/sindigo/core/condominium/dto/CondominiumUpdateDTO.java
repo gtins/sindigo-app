170cc83afcfc6db1f31f06e8fe59d4258f769f14
Gustavo H. Martins <gusmartins06@gmail.com>
2026-06-16 13:15:21 -0300

    feat: Adicionando opções de editar e inativar condomínios.

---
+++ src/main/java/com/api/sindigo/core/condominium/dto/CondominiumUpdateDTO.java
@@ -0,0 +1,30 @@
+package com.api.sindigo.core.condominium.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CondominiumUpdateDTO {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Address is required")
+    private String address;
+
+    @NotNull(message = "Unidades são obrigatórias")
+    private Integer unidades;
+
+    @NotNull(message = "Active status is required")
+    private Boolean active;
+}
+
